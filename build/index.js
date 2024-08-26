a45cbd095ebdb84a092d0cbd5357b24dac7fb036
moriDumb <waytau71@gmail.com>
2024-08-26 21:17:55 +0800

    damn

--- build/index.js
+++ build/index.js
@@ -3,7 +3,7 @@ const konh = document.getElementById("konhp")
 const konten1 = document.getElementById("konten1")
 
 konh.addEventListener("mouseenter", (event) => {
-   const kop = konhp.style.display = "block";
+  const kop = konhp.style.display = "block";
    const kont = konten1.style.filter = "blur(0px)"
 
     setTimeout(() =>{
